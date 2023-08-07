9656d0c6bc5caf697706cc4b888a8b21b92ac167
codeschris <chrismlolwa42@outlook.com>
2023-08-07 23:07:23 +0300

    Changes - Backend dev begins - Database creation

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, render_template
 from flask_bcrypt import Bcrypt
+import sqlite3
 
 app = Flask(__name__)
 bcrypt = Bcrypt(app)
@@ -14,7 +15,28 @@ def verify_password(password, hashed_password):
 
 @app.route('/', methods=['GET', 'POST'])
 def login():
-    return render_template('index.html')
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+
+        try:
+            conn = sqlite3.connect('database.db')
+            cur = conn.cursor()
+            query = "SELECT username, pass_word FROM student where name = ?;"
+            cur.execute(query, (username, ))
+
+            if password == password:
+                return render_template('welcome.html')
+            
+        except sqlite3.Error as error:
+            print('Error occurred - ', error)
+
+        finally:
+            cur.close()
+            conn.close()
+    
+    return render_template("index.html")
+            
 
 if __name__ == '__main__':
     app.run(debug=True)
