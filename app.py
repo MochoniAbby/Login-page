747b82c7b3bf49a298c92d85cf7be7c9dc1d0487
MochoniAbby <abigaelmoraa24@gmail.com>
2023-08-07 10:24:15 +0300

    Changes - Created a new branch - Created folders for respective files and elements to be used

--- app.py
+++ app.py
@@ -1,1 +1,13 @@
-from flask import Flask
+from flask import Flask, request, redirect
+from flask_bcrypt import Bcrypt
+
+app = Flask(__name__)
+bcrypt = Bcrypt(app)
+
+#Hashing the password
+def hash_password(password):
+    return bcrypt.generate_password_hash(password).decode('utf-8')
+
+#Comparing the hashed password and the one in the database
+def verify_password(password, hashed_password):
+    return bcrypt.check_password_hash(password, hashed_password.encode('utf-8'))
