a06ae31f4a60a54a67ba3587b850f967029230eb
MochoniAbby <abigaelmoraa24@gmail.com>
2023-08-09 11:02:48 +0300

    Changes - Deleting database entry - Updating schema

--- schema.sql
+++ schema.sql
@@ -1,4 +1,4 @@
-DROP TABLE IF EXISTS student_new;
+-- DROP TABLE IF EXISTS student_new;
 
 /*CREATE TABLE student (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -6,11 +6,4 @@ DROP TABLE IF EXISTS student_new;
     pass_word TEXT NOT NULL
 );*/
 
-CREATE TABLE student_new (
-    username TEXT PRIMARY KEY, 
-    pass_word VARCHAR(255),
-    email TEXT NOT NULL, 
-    otp_code TEXT
-);
-
-ALTER TABLE student_new RENAME TO student;
+-- DELETE FROM student WHERE username = "Chris";
